d3f6b61328eea6e6f296e978dfc258316949b31c
Hadi El-Yakhni <57732964+Hadi-El-Yakhni@users.noreply.github.com>
2020-01-21 19:34:07 +0200

    remove ci from .gitignore

---
+++ config/ci.js
@@ -0,0 +1,7 @@
+module.exports = {
+    googleClientID: '694616207013-0sl3lsj5q3njiven9pev4julftedl0gn.apps.googleusercontent.com',
+    googleClientSecret: 'GQZELcbtxupnoPUiM8sRIgA9',
+    mongoURI: 'mongodb://127.0.0.1:27017/blog_everyone_ci',
+    cookieKey: 'sdfghlsdiubsdakguakljhgdfaoityf9o7874tuhjgio7gcyuifilsdguf',
+    redisUrl: 'redis://127.0.0.1:6379'
+}
